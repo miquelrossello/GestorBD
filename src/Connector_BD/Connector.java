41ad98baf23eabcb3bfa28fe7feb15cb13cdfec6
Miquel <rossellomelismiquel@gmail.com>
2018-05-04 11:34:12 +0200

    FXML Login canvi

--- src/Connector_BD/Connector.java
+++ src/Connector_BD/Connector.java
@@ -1,4 +1,11 @@
 package Connector_BD;
 
 public class Connector {
+
+    private String username, password, database;
+
+    public Connector(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
