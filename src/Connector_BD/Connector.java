b0cb755c25db55b877307ffd5f9fb6ae2a31ea44
Miquel <rossellomelismiquel@gmail.com>
2018-05-05 15:16:37 +0200

    Connector BD creat

--- src/Connector_BD/Connector.java
+++ src/Connector_BD/Connector.java
@@ -1,11 +1,29 @@
 package Connector_BD;
 
+import java.sql.*;
+
 public class Connector {
 
-    private String username, password, database;
+    public static Connection createConnection(String username, String password) {
+        String url = "jdbc:mysql://localhost/contactes";
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
 
-    public Connector(String username, String password) {
-        this.username = username;
-        this.password = password;
+class TestSQL {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = Connector.createConnection("root", "mrm1998");
+        Statement stmnt = conn.createStatement();
+        ResultSet rS = stmnt.executeQuery("SHOW DATABASES");
+        while (rS.next()) {
+            System.out.println(rS.getString(1));
+        }
     }
 }
