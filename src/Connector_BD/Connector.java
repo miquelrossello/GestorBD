f1676ae358c7342d273c3b6b7df1153c4f78cc42
newtimestube <newtimestube@gmail.com>
2018-05-05 22:40:51 +0200

    Connection DB

--- src/Connector_BD/Connector.java
+++ src/Connector_BD/Connector.java
@@ -1,11 +1,26 @@
 package Connector_BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Connector {
 
     private String username, password, database;
+    public Connection connection;
 
-    public Connector(String username, String password) {
+    public Connection getConnection(String username, String password, String database, String server) {
+
+        this.database = database;
         this.username = username;
         this.password = password;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database, username, password);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return connection;
     }
 }
--- src/login/ControllerLogin.java
+++ src/login/ControllerLogin.java
@@ -1,12 +1,43 @@
 package login;
 
+import Connector_BD.Connector;
 import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import org.w3c.dom.Text;
 
-public class ControllerLogin extends Application
-{
+import java.sql.Connection;
+
+public class ControllerLogin extends Application {
+    public TextField server;
+    public TextField database;
+    public TextField user;
+    public TextField pass;
+    public Label connection;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+    }
+
+    public void loginButton(ActionEvent actionEvent) {
+        Connector connector = new Connector();
+        Connection connections = connector.getConnection(changeType(user), changeType(pass), changeType(database), changeType(server));
+
+        if (connections != null) {
+            connection.setText("SUCCESFULL");
+        } else {
+            connection.setText("FAILED");
+        }
+    }
+
+    public void registerButton(ActionEvent actionEvent) {
+        connection.setText(pass.getText());
+    }
+
+    private String changeType(TextField text){
+        return (String) text.getText();
     }
 }
