51e83c75ac544acd9cf1825d5bea93095705fccc
miquelrossello <rossellomelismiquel@gmail.com>
2018-05-10 12:22:43 +0200

    Canvi TableView a ListView

--- src/app/ControllerApp.java
+++ src/app/ControllerApp.java
@@ -6,9 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.ListView;
 
 import java.net.URL;
 import java.sql.ResultSet;
@@ -19,31 +17,37 @@ import java.util.ResourceBundle;
 public class ControllerApp implements Initializable {
 
     @FXML
-    private TableView<Database> databasesTable;
-    @FXML
-    private TableColumn<Database, String> databasesName;
+    private ListView<String> databasesList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        databasesName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        databasesTable.setItems(getDatabases());
+        setTableList();
+        databasesList.setItems(getDatabases());
     }
 
-    private ObservableList<Database> getDatabases() {
+    private void setTableList() {
+        ObservableList<String> dbList = databasesList.getItems();
+        for (int x = 0; x < dbList.size(); x++) {
+
+        }
+    }
+
+    private ObservableList<String> getDatabases() {
         String userDB = Session.getInstance().getUsername();
         String passDB = Session.getInstance().getPassword();
-        ObservableList<Database> databases = FXCollections.observableArrayList();
+        ObservableList<String> databases = FXCollections.observableArrayList();
         try {
             Connector conn = Connector.getInstance();
             conn.connectar(userDB, passDB, "");
             Statement statement = conn.getConnection().createStatement();
             ResultSet rS = statement.executeQuery("SHOW DATABASES");
             while (rS.next()) {
-                databases.add(new Database(rS.getString(1)));
+                databases.add(new Database(rS.getString(1)).getName());
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return databases;
     }
+
 }
